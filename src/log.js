665268b9c62fdf7e679d47ae6c37d4e8ac10ae89
ntibi <nicolastibi@yahoo.fr>
2020-02-17 11:45:09 +0100

    logs

---
+++ src/log.js
@@ -0,0 +1,14 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+  defaultMeta: { service: 'web' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'out.log' }),
+    new winston.transports.Console({ format: winston.format.simple() }),
+  ]
+});
+
+module.exports = logger;
